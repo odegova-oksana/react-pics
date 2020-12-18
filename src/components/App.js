1ac91b75b6769f437d1c740ba93824c2d5c73787
Oxana Odegova <oxyapple@yandex.ru>
2020-12-18 14:55:22 +0100

    add async with requests

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    axios
-      .get ('https://api.unsplash.com/search/photos', {
-        params: {query: term},
-        headers: {
-          Authorization: 'Client-ID usRka9cePWCMVJnYVP2ALGEklwvlInmMirEVrdqtFnE',
-        },
-      })
-      .then (response => {
-        console.log (response.data.results);
-      });
-  };
+  async onSearchSubmit (term) {
+    const response = await axios.get ('https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization: 'Client-ID usRka9cePWCMVJnYVP2ALGEklwvlInmMirEVrdqtFnE',
+      },
+    });
+
+    console.log(response.data.results);
+  }
 
   render () {
     return (
