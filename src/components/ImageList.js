8390a269058050321bacc0951826e3b664d967df
Oxana Odegova <oxyapple@yandex.ru>
2020-12-19 20:33:12 +0100

    add grip and spans

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard.js';
 
 const ImageList = props => {
-  const images = props.images.map (({description, id, urls}) => (
-    <img alt={description} key={id} src={urls.small} />
+  const images = props.images.map ((image) => (
+    <ImageCard key={image.id} image={image}/>
   ));
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
